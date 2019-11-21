b058d88c7ba0dcdf8d64e74efbe1d5c9e84bf4f0
Adarsh64 <adarshnayak64@gmail.com>
2019-11-21 23:46:09 +0530

    added hotels api service

--- server/food-forks-server.py
+++ server/food-forks-server.py
@@ -388,6 +388,10 @@ def booktables():
 	
 	return jsonify({"tables":output})
 
+@app.route('/api/v1/hotels',methods=['GET'])
+def getHotels():
+    res =  [{"name": "abc","ratings": 3.8,"image": "im","description": "fast food"},{"name": "def","ratings": 4.8,"image": "im","description": "fast food"},{"name": "ghi","ratings": 3.8,"image": "im","description": "fast food"},{"name": "klj","ratings": 3.8,"image": "im","description": "fast food"}]
+    return jsonify({"hotels":res})
 
 @app.route("/")
 def home():
